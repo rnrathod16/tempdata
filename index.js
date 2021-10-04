320e34aa0697004443bcb967ae5394fe1973977d
rnrathod16 <rnrathod16@gmail.com>
2021-10-04 13:43:36 +0530

    merg2\3

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ app.use(require('./routes/route'))
 if (process.env.NODE_ENV == "production") {
     app.use(express.static("frontend/build"));
     const path = require("path");
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
 }
 
 const PORT = process.env.PORT || 5000;
